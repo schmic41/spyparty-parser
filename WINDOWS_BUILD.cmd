14e681ed1007dfe97d7867d193deda63acd2beb9
Cameron Schmidt <schmic41@gmail.com>
2021-06-09 19:36:25 -0700

    Small change to the build script

--- WINDOWS_BUILD.cmd
+++ WINDOWS_BUILD.cmd
@@ -1,5 +1,5 @@
 raco make "src/spyparty_replay_parser.rkt" "src/glicko.rkt" "src/helpers.rkt" "src/parser.rkt"
 
-raco exe -o "SpyParty_Replay_Viewer" --cs --gui --ico "resources/favicon.ico" --embed-dlls "src/spyparty_replay_parser.rkt"
+raco exe -o "SpyParty_Replay_Viewer" --gui --ico "resources/favicon.ico" --embed-dlls "src/spyparty_replay_parser.rkt"
 
 raco distribute "SpyParty_Replay_Viewer" SpyParty_Replay_Viewer.exe
